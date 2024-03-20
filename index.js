a0b7dd81e6334a2670e1b4434c074db2daaea151
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-03-20 17:43:55 +0300

    adds functions and conditions to them

--- index.js
+++ index.js
@@ -8,18 +8,10 @@ function distanceFromHqInBlocks(dist) {
         return blocks
     }
 }
-const distanceFromHqInFeet = dist => {
-    if (dist > 42) {
-        let blocks = dist - 42
-        let feet = blocks * 264
-        return feet
-    }
-    else {
-        let blocks = 42 - dist
-        let feet = blocks * 264
-        return feet
-    }
-}
+function distanceFromHqInFeet(dist){
+    return distanceFromHqInBlocks(dist) * 264
+
+} 
 function distanceTravelledInFeet(num1, num2) {
     if (num2 > num1) {
         let distTravelled = (num2 - num1)*264
@@ -31,38 +23,21 @@ function distanceTravelledInFeet(num1, num2) {
     }
 }
 const calculatesFarePrice = (start, destination) => {
-    if (destination > start) {
-        let distTravelled = (destination - start)*264
+        let distTravelled = distanceTravelledInFeet(start, destination)
         if (distTravelled < 400) {
             let cost = 0
             return cost
         }
         if (distTravelled >400, distTravelled < 2000) {
-            let cost = distTravelled * 0.02
-            return cost
-        }
-        else {
-            let cost = 25
-            return cost
-        }
-    }
-    else {
-        let distTravelled = (start- destination)*264
-        if (distTravelled < 400) {
-            let cost = 0
-            return cost
-        }
-        else if (distTravelled >400, distTravelled < 2000) {
             let cost = (distTravelled-400) * 0.02
             return cost
+            
         }
-        else if (distTravelled >2000, distTravelled < 2500) {
+        else if (distTravelled > 2000, distTravelled< 2500){
             let cost = 25
             return cost
         }
         else {
             return "cannot travel that far"
         }
-
     }
-}
